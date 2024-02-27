b6e9d5c5e8d114fb443cb8f04910ee48c6a92fdb
israel4u <israeloluwasholao@gmail.com>
2024-02-27 08:12:03 +0100

    The first major refactoring with router class.

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
 require '../helpers.php';
 
-$routes = basePath('routes.php');
+
+require basePath('Router.php');
+
+$router = new Router();
+
+$routes = require basePath('routes.php');
 
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-require basePath('router.php');
+$router->route($uri, $method);
